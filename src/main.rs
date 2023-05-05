4e93c04d6d80333506a3745176536cd17bace0aa
chenrongfeng <chenrongfeng@yolonet.io>
2023-05-05 16:14:33 +0800

    fix: remove config

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,11 @@
 use std::net::TcpListener;
 use sentry_webhook::{
     startup::{run},
-    config::get_config,
 };
 
 #[tokio::main]
 async fn main() -> Result<(), hyper::Error> {
-    let config = get_config().expect("Cannot read configuration");
-    let socket_addr = format!("127.0.0.1:{}", config.application_port);
+    let socket_addr = format!("127.0.0.1:{}", "9230");
     let addr = socket_addr.clone();
     println!("Listening on {}", addr);
     let listener = TcpListener::bind(socket_addr).expect("Cannot bind to socket");
